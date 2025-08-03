f79499ed8320917cb92fdb0085407f94678e40b2
SHASHIKANT KUMAR <sasikant297@gmail.com>
2025-08-03 11:34:27 +0530

    This is commit of file daypractice day1 and1.2

---
+++ daypractice/day1.2.py
@@ -0,0 +1,6 @@
+#Eeay sum of two variable
+a=10
+b=10.5
+c=a+b
+print("sum : ", c)
+print("Python is a high-level, general-purpose programming language. Its design philosophy emphasizes code readability with the use of significant indentation.[34]Python is dynamically type-checked and garbage-collected. It supports multiple programming paradigms, including structured (particularly procedural), object-oriented and functional programming.")
