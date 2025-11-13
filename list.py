8183049a8cc4800f98b950a24b09f893a3746958
SHASHIKANT KUMAR <sasikant297@gmail.com>
2025-11-13 07:58:09 +0530

    initial commit

---
+++ list.py
@@ -0,0 +1,9 @@
+countries = ['UK', 'India', 'Bhutan', 'Srilanka', 'Japan']
+print(countries[2][3])
+# range
+print(countries[1:3])
+# type of datatype
+print(type(countries))
+
+countries[0]=Canada
+print()
