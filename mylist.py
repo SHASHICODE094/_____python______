9fe06749e08ab3efaa438912d0f26436f5810686
SHASHIKANT KUMAR <sasikant297@gmail.com>
2026-04-21 07:33:21 +0530

    This is the second commit to the file

--- mylist.py
+++ mylist.py
@@ -1,11 +1,11 @@
 mylist = ['computer', 'hardware', 'software', 'os']
 # for i in mylist:
 #     print(i)
-input_choice = input("Enter your choice : ")
-if input_choice in mylist:
-    print("available")
-else:
-    print("not available")
+# input_choice = input("Enter your choice : ")
+# if input_choice in mylist:
+#     print("available")
+# else:
+#     print("not available")
 print(mylist)
 # print(mylist[1])
 # if input_choice in mylist:
