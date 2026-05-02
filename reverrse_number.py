dabfeee867d0d8d72f784c9066178148e479c41c
SHASHIKANT KUMAR <sasikant297@gmail.com>
2026-05-02 06:34:04 +0530

    Initial commit

---
+++ reverrse_number.py
@@ -0,0 +1,16 @@
+# slicing method----------
+# text= "My name is Python "
+# reverse_text= text[:: -1]
+# print(reverse_text)
+
+#readable way--------
+# text = "This is python"
+# reverse_text = "".join(reversed(text))
+# print(reverse_text)
+
+# using loop---------
+text= "This is my python"
+res = " "
+for char in text:
+    res = char+res
+print(res)
