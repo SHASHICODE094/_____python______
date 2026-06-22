28b730c77c71ed14b86ac1b20c09874d102c01a8
SHASHIKANT KUMAR <sasikant297@gmail.com>
2026-06-22 09:37:25 +0530

    2nd commit change some variable name

--- pthon_Project/quiz.py
+++ pthon_Project/quiz.py
@@ -1,10 +1,11 @@
-r=0
-y= { 1:" 1.Mumbai"" 2.Channai"" 3.Kolkata"" 4.New Delhi", 2:"1. 1947 2.1936 3.1974 4.1911", 3:"1.programming language 2.language 3.Machime language", 4:"1.Variable 2.datatype 3. function 4.structure", 5:"Type of data 2.function"}
-q={1:"1.What is  the  capital citu of India?", 2:"2.When did India get independent", 3:"3.What  is python?", 4:"4.What is bool?", 5:"5.What is the datatype?"}
+# This is a quiz code in which the developer tries to make a quiz setup for user where they entered  the  answer and get score according to their first attempt. It offers user  to enter the right  answer but the score depend on first step. This is  not fully accurate. The  developer tries to put his knowledge.
+score=0
+option= { 1:" 1.Mumbai"" 2.Channai"" 3.Kolkata"" 4.New Delhi", 2:"1. 1947 2.1936 3.1974 4.1911", 3:"1.programming language 2.language 3.Machime language", 4:"1.Variable 2.datatype 3. function 4.structure", 5:"Type of data 2.function"}
+question={1:"1.What is  the  capital citu of India?", 2:"2.When did India get independent", 3:"3.What  is python?", 4:"4.What is bool?", 5:"5.What is the datatype?"}
 ans={1:"New Delhi", 2:"1947", 3:"programming language", 4:"datatype", 5:"Type of data"}
-for i in y:
-    print(q[i])
-    print(y[i])
+for i in option:
+    print(question[i])
+    print(option[i])
     answer= ans[i]
     # print(answer)
     while True:
@@ -14,10 +15,10 @@ for i in y:
        print("Your answer :",value)
      elif value!=answer:
        print("->You have  entered the wrong answer.Enter  the right answer")
-       r=r-1
+       score=score-1
      else:
        print("->You have enter right answer!!!!!!!")
-       r=r+1
+       score=score+1
        break
      
-print("score: ", r)
+print(score)
