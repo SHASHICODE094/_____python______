8c462d0476be7655f0647ba834e2d51e462a4816
SHASHIKANT KUMAR <sasikant297@gmail.com>
2026-06-29 22:13:38 +0530

    Initial commit, This is calculator program using module

---
+++ Calculator/calculate.py
@@ -0,0 +1,10 @@
+def addition(n, m):
+    return print(f"The sum of {n} and {m} are {n+m}")
+
+def subtraction(n, m):
+    return print(f"The subtract of {n} and {m} are {n-m}")
+
+def multiplication(n, m):
+    return print(f"The multiple of {n} and {m} are {n*m}")
+def divission(n, m):
+    return print(f"The division of {n} and {m} are {n/m}")
---
+++ Calculator/calculate_use.py
@@ -0,0 +1,44 @@
+import calculate
+print("The Calculator ")
+value1= int(input("Enter the 1st number : "))
+value2= int(input("Enter the 2nd number : "))
+calculate.addition(value1, value2)
+calculate.subtraction(value1, value2)
+calculate.multiplication(value1, value2)
+calculate.divission(value1, value2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
