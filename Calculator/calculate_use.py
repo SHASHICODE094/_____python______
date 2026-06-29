19a106957dea7c12d3eb59b4cb021387be3d7fbd
SHASHIKANT KUMAR <sasikant297@gmail.com>
2026-06-29 22:21:50 +0530

    2nd commit

--- Calculator/calculate_use.py
+++ Calculator/calculate_use.py
@@ -2,10 +2,20 @@ import calculate
 print("The Calculator ")
 value1= int(input("Enter the 1st number : "))
 value2= int(input("Enter the 2nd number : "))
-calculate.addition(value1, value2)
-calculate.subtraction(value1, value2)
-calculate.multiplication(value1, value2)
-calculate.divission(value1, value2)
+print("__-----OPTION-----__")
+print("1. Addition")
+print("2. Subtractio")
+print("3. Multiplication")
+print("4. Division")
+choice=int(input("Enter your choice! "))
+if choice==1:
+    calculate.addition(value1, value2)
+elif choice==2:
+    calculate.subtraction(value1, value2)
+elif choice==3:
+    calculate.multiplication(value1, value2)
+elif choice==4:
+    calculate.divission(value1, value2)
 
 
 
